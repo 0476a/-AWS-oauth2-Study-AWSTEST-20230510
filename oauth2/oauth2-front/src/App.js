c9f60f71c79e5af23c7415c8a88e2ecef6a59486
0476a <0476a@naver.com>
2023-05-22 17:41:44 +0900

    2023.05.22(비밀번호 찾기 요청 메일 보내기)

--- oauth2/oauth2-front/src/App.js
+++ oauth2/oauth2-front/src/App.js
@@ -8,20 +8,22 @@ import Index from './pages/index/Index';
 import OAuth2Login from './pages/Login/OAuth2Login';
 import AuthRoute from './components/auth/AuthRoute';
 import PostRegister from './pages/Posts/PostRegister';
+import ForgotPassword from './pages/Forgot/ForgotPassword';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<AuthRoute path={"/"} element={<Index/>}/>}/>
-        <Route path='/mypage' element={<AuthRoute path={"/mypage"} element={<Index/>}/>}/>
-        <Route path='/post/register' element={<AuthRoute path={"/post/register"} element={<PostRegister/>}/>}/>
-        <Route path='/auth/login' element = {<AuthRoute path={"/auth/login"} element={<Login/>}/>} />
+        <Route path='/auth/forgot/password' element={<AuthRoute path={"/auth/forgot/password"} element={<ForgotPassword />} />} />
+        <Route path='/' element={<AuthRoute path={"/"} element={<Index />} />} />
+        <Route path='/mypage' element={<AuthRoute path={"/mypage"} element={<Index />} />} />
+        <Route path='/post/register' element={<AuthRoute path={"/post/register"} element={<PostRegister />} />} />
+        <Route path='/auth/login' element={<AuthRoute path={"/auth/login"} element={<Login />} />} />
         <Route path='/auth/register' />
-        <Route path='/auth/oauth2/login' element={<AuthRoute path={"/auth/oauth2/login"} element={<OAuth2Login />}/>}/>
-        <Route path='/auth/oauth2/register' element={<AuthRoute path={"/auth/oauth2/register"} element={<OAuth2Register />}/>}/>
-        <Route path='/auth/oauth2/merge' element={<AuthRoute path={"/auth/oauth2/merge"} element={<OAuth2Merge />}/>}/>
-        <Route path='/*' element={<NotFound/>}/>
+        <Route path='/auth/oauth2/login' element={<AuthRoute path={"/auth/oauth2/login"} element={<OAuth2Login />} />} />
+        <Route path='/auth/oauth2/register' element={<AuthRoute path={"/auth/oauth2/register"} element={<OAuth2Register />} />} />
+        <Route path='/auth/oauth2/merge' element={<AuthRoute path={"/auth/oauth2/merge"} element={<OAuth2Merge />} />} />
+        <Route path='/*' element={<NotFound />} />
       </Routes>
     </>
   );
--- oauth2/oauth2-front/src/pages/Login/Login.js
+++ oauth2/oauth2-front/src/pages/Login/Login.js
@@ -7,18 +7,17 @@ const Login = () => {
 
     const googleAuthClickHandle = () => {
         window.location.href = `http://localhost:8080/oauth2/authorization/google`;
-        
     }
 
     const naverAuthClickHandle = () => {
         window.location.href = `http://localhost:8080/oauth2/authorization/naver`;
-        
+
     }
 
     return (
         <div>
-            <input type="text" placeholder='email'/>
-            <input type="password" placeholder='password'/>
+            <input type="text" placeholder='email' />
+            <input type="password" placeholder='password' />
             <button>로그인</button>
             <button onClick={googleAuthClickHandle}><FcGoogle /></button>
             <button onClick={naverAuthClickHandle}>네이버</button>
